f47242ef69bcc171a82b906eccb825da9386aa58
Ajzboss <ajano3191@gmail.com>
2024-03-21 03:11:01 -0700

    Added Changes From Broken Branch to CleanedMain
    I ran into issues with the previous branch,
    due to  large file being saved in a local commit
    history. I have manually saved the changes in
    a local directory and then manually changed the
    main brnach to remove the large file. Now we
    should have a clean main branch history with the
    most recent changes from the broken branch.

--- NNDL/Utils/solver.py
+++ NNDL/Utils/solver.py
@@ -6,9 +6,14 @@ from torchvision.transforms import ToTensor
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import tensorflow as tf
 from torch.utils.tensorboard import SummaryWriter
-
-# default `log_dir` is "runs" - we'll be more specific here
+import numpy as np
+#for confusion matrix
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import matplotlib.pyplot as plt
 
 device = (
     "cuda"
@@ -17,24 +22,20 @@ device = (
     if torch.backends.mps.is_available()
     else "cpu"
 )
-#device="cpu"
+#APply this if you want to make your model cpu or gpu
+device="cpu" 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     model.train()
-    for batch, (X, y) in enumerate(dataloader):
+    for batch, (X,X_img, y) in enumerate(dataloader):
         #print(X)
         X, y = X.to(device), y.to(device)
         if torch.isnan(X).any() or torch.isinf(X).any():
                 raise ValueError("Input data contains NaN or infinite values.")
         # Compute prediction error
         pred = model(X)
-        #print(pred.shape)
-        #print(y.shape)
-        loss = loss_fn(pred, y)
-        #loss = loss.clamp(min=1e-4)
-        # cm = confusion_matrix(y, pred)
-        # ConfusionMatrixDisplay(cm, model.classes_).plot()
 
+        loss = loss_fn(pred, y)
         # Backpropagation
         loss.backward()
         # for param in model.parameters():
@@ -50,12 +51,12 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 128 == 0:
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-            if torch.cuda.is_available():
-                torch.cuda.empty_cache()
-                print("Using GPU:", torch.cuda.get_device_name(device))
-                print("GPU Memory Usage:")
-                print("Allocated:", round(torch.cuda.memory_allocated(device)/1024**3,1), "GB")
-                print("Cached:   ", round(torch.cuda.memory_reserved(device)/1024**3,1), "GB")
+            # if torch.cuda.is_available():
+            #     torch.cuda.empty_cache()
+            #     print("Using GPU:", torch.cuda.get_device_name(device))
+            #     print("GPU Memory Usage:")
+            #     print("Allocated:", round(torch.cuda.memory_allocated(device)/1024**3,1), "GB")
+            #     print("Cached:   ", round(torch.cuda.memory_reserved(device)/1024**3,1), "GB")
         del loss,current,pred
     #print("Batching Complete")
 
@@ -67,18 +68,203 @@ def test(dataloader, model, loss_fn):
         num_batches = len(dataloader)
         test_loss, correct = 0, 0
         iter=0
-        for X, y in dataloader:
-            #print("Testing Prediction")
+        all_pred=None
+        all_labels=None
+        i = None
+        for X,X_img,y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
+            if i == None:
+                all_pred=np.argmax(np.array(pred), axis=1)
+                all_labels =np.array(y)
+                i=True
+            else:
+                all_pred= np.concatenate((all_pred,np.argmax(np.array(pred), axis=1)),axis=0)
+                all_labels = np.concatenate((all_labels,np.array(y)),axis=0)
+            test_loss += loss_fn(pred, y)
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            iter+= 1 
+            print(f"Progress: {iter}/{num_batches},accuracy:{(100*(correct/size)):>0.1f},test_loss:{(test_loss/iter):>8f}")
+            if iter % 100==0:
+                print(f"Progress: {iter}/{num_batches},accuracy:{(100*(correct/size)):>0.1f},test_loss:{(test_loss/iter):>8f}")
+        test_loss /= num_batches
+        correct /= size
+        cm = confusion_matrix(all_labels,all_pred)
+        class_names=['arts, crafts & sewing','books', 'clothing, shoes & jewelry','electronics', 'grocery & gourmet food','health & personal care', 'musical instruments', 'patio, lawn & garden','sports & outdoors','toys & games']
+        confusion_df = pd.DataFrame(cm, index=class_names, columns=class_names)
+        confusion_df.index.name = 'True Label'
+        confusion_df.columns.name = 'Predicted Label'
+        sns.heatmap(confusion_df, annot=True, fmt='3g')
+        plt.show()
+        print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+def test_ensemble(dataloader, model_list, loss_fn,t_model,weights=[0.5,0.5]):
+    for model in model_list:
+        model.eval()
+    print("Beginnning inference")
+    all_pred=None
+    all_labels=None
+    i = None
+    with torch.no_grad():
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        test_loss, correct = 0, 0
+        iter=0
+        for X_text,X_img,y in dataloader:
+            X_img=torch.transpose(X_img,1,3)
+            X_img = X_img.numpy()
+            X_img = tf.convert_to_tensor(X_img)
+            X_img=tf.cast(X_img,tf.float32)
+            X_img=tf.keras.applications.inception_v3.preprocess_input(X_img)
+            print("Testing Prediction")
+            X_text, y = X_text.to(device),y.to(device)
+            pred_by_model = []
+            #print(X_text)
+            for model in model_list:
+                out = model(X_text)
+                pred_by_model.append(out)
+            t_out=t_model.predict(X_img)
+            t_out=torch.tensor(t_out)
+            pred_by_model.append(t_out)
+            for it,weight in enumerate(weights):
+                pred_by_model[it]*=weight
+            stacked_tensor = torch.stack(pred_by_model)
+            pred = torch.sum(stacked_tensor,axis=0)
+            if i == None:
+                all_pred=np.argmax(np.array(pred), axis=1)
+                all_labels =np.array(y)
+                i=True
+            else:
+                all_pred= np.concatenate((all_pred,np.argmax(np.array(pred), axis=1)),axis=0)
+                all_labels = np.concatenate((all_labels,np.array(y)),axis=0)
             #print(pred)
             #print(y)
             test_loss += loss_fn(pred, y)
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
             iter+= 1 
+            print(f"Progress: {iter}/{num_batches},accuracy:{(100*(correct/size)):>0.1f},test_loss:{(test_loss/iter):>8f}")
+
             if iter % 100==0:
                 print(f"Progress: {iter}/{num_batches},accuracy:{(100*(correct/size)):>0.1f},test_loss:{(test_loss/iter):>8f}")
     #print("completed inference")
         test_loss /= num_batches
         correct /= size
+        cm = confusion_matrix(all_labels,all_pred)
+        class_names=['arts, crafts & sewing','books', 'clothing, shoes & jewelry','electronics', 'grocery & gourmet food','health & personal care', 'musical instruments', 'patio, lawn & garden','sports & outdoors','toys & games']
+        confusion_df = pd.DataFrame(cm, index=class_names, columns=class_names)
+        confusion_df.index.name = 'True Label'
+        confusion_df.columns.name = 'Predicted Label'
+        #print(confusion_df)
+        sns.heatmap(confusion_df, annot=True, fmt='3g')
+        plt.show()
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+def train_stack(dataloader, model_list,loss_fn, optimizer,meta_model,t_model):
+    print("Training Meta Model")
+    size = len(dataloader.dataset)
+    for model in model_list:
+        model.eval()
+    meta_model.train()
+    for batch, (X_text,X_img, y) in enumerate(dataloader):
+        #print(X)
+        X_text, y = X_text.to(device),y.to(device)
+        X_img=torch.transpose(X_img,1,3)
+        X_img = X_img.numpy()
+        X_img = tf.convert_to_tensor(X_img)
+        X_img=tf.cast(X_img,tf.float32)
+        X_img=tf.keras.applications.inception_v3.preprocess_input(X_img)
+        # Compute prediction error
+        pred_by_model = []
+        for model in model_list:
+            pred_by_model.append(model(X_text))
+        t_out=t_model.predict(X_img)
+        t_out=torch.tensor(t_out)
+        pred_by_model.append(t_out)
+        stacked_tensor = torch.stack(pred_by_model)
+        pred_by_model=torch.transpose(stacked_tensor,1,0)
+        pred = meta_model(pred_by_model)
+        #pred=torch.transpose(pred)
+        loss = loss_fn(pred, y)
+        # Backpropagation
+        loss.backward()
+        # for param in model.parameters():
+        #     if param.grad is not None:
+        #         if torch.isnan(param.grad).any() or torch.isinf(param.grad).any():
+        #             print(f"Param.grad that is nan or inf:{param.grad} ")
+        #torch.nn.utils.clip_grad_norm_(model.parameters(),10,error_if_nonfinite =True)
+        optimizer.step()
+        optimizer.zero_grad()
+        #print(loss)
+        loss, current = loss.item(), (batch + 1) * (len(X_text))
+        #print(f"loss: {loss}  [{current}/{size}]")
+        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        if batch % 128 == 0:
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            # if torch.cuda.is_available():
+            #     torch.cuda.empty_cache()
+            #     print("Using GPU:", torch.cuda.get_device_name(device))
+            #     print("GPU Memory Usage:")
+            #     print("Allocated:", round(torch.cuda.memory_allocated(device)/1024**3,1), "GB")
+            #     print("Cached:   ", round(torch.cuda.memory_reserved(device)/1024**3,1), "GB")
+        del loss,current,pred
+    #print("Batching Complete")
+
+def test_stack(dataloader, model_list, loss_fn,meta_model,t_model):
+    for model in model_list:
+        model.eval()
+    meta_model.eval()
+    print("Beginnning inference")
+    with torch.no_grad():
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        test_loss, correct = 0, 0
+        iter=0
+        all_pred=None
+        all_labels=None
+        i = None
+        for X_text,X_img,y in dataloader:
+            X_img=torch.transpose(X_img,1,3)
+            X_img = X_img.numpy()
+            X_img = tf.convert_to_tensor(X_img)
+            X_img=tf.cast(X_img,tf.float32)
+            X_img=tf.keras.applications.inception_v3.preprocess_input(X_img)
+            X_text, y = X_text.to(device),y.to(device)
+            pred_by_model = []
+            #print(X_text)
+            for model in model_list:
+                out = model(X_text)
+                pred_by_model.append(out)
+            t_out=t_model.predict(X_img)
+            t_out=torch.tensor(t_out)
+            pred_by_model.append(t_out)
+            stacked_tensor = torch.stack(pred_by_model)
+            pred_by_model=torch.transpose(stacked_tensor,1,0)
+            pred = meta_model(pred_by_model)
+            if i == None:
+                all_pred=np.argmax(np.array(pred), axis=1)
+                all_labels =np.array(y)
+                i=True
+            else:
+                all_pred= np.concatenate((all_pred,np.argmax(np.array(pred), axis=1)),axis=0)
+                all_labels = np.concatenate((all_labels,np.array(y)),axis=0)
+            #print(pred)
+            #print(y)
+            test_loss += loss_fn(pred, y)
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            iter+= 1 
+            print(f"Progress: {iter}/{num_batches},accuracy:{(100*(correct/size)):>0.1f},test_loss:{(test_loss/iter):>8f}")
+            if iter % 100==0:
+                print(f"Progress: {iter}/{num_batches},accuracy:{(100*(correct/size)):>0.1f},test_loss:{(test_loss/iter):>8f}")
+    #print("completed inference")
+        test_loss /= num_batches
+        correct /= size
+        cm = confusion_matrix(all_labels,all_pred)
+        class_names=['arts, crafts & sewing','books', 'clothing, shoes & jewelry','electronics', 'grocery & gourmet food','health & personal care', 'musical instruments', 'patio, lawn & garden','sports & outdoors','toys & games']
+        confusion_df = pd.DataFrame(cm, index=class_names, columns=class_names)
+        confusion_df.index.name = 'True Label'
+        confusion_df.columns.name = 'Predicted Label'
+        #print(confusion_df)
+        sns.heatmap(confusion_df, annot=True, fmt='3g')
+        plt.show()
+        print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
